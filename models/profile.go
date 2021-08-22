bbc6751fc4a3e54eccc7966f03221b2f4497adcc
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-22 21:30:44 +0900

    Create Profile model

---
+++ models/profile.go
@@ -0,0 +1,35 @@
+package models
+
+// Profile is struct of profile
+type Profile struct {
+	FamilyNameKanji string `json:"family_name_kanji"`
+	GivenNameKanji  string `json:"given_name_kanji"`
+	FamilyNameKana  string `json:"family_name_kana"`
+	GivenNameKana   string `json:"given_name_kana"`
+	FamilyNameEn    string `json:"family_name_en"`
+	GivenNameEn     string `json:"given_name_en"`
+	Nickname        string `json:"nickname"`
+	ImageURL        string `json:"image_url"`
+	Job             string `json:"job"`
+	Email           string `json:"email"`
+	Bio             string `json:"bio"`
+	Location        string `json:"location"`
+}
+
+// GetProfile returns a profile
+func GetProfile() *Profile {
+	return &Profile{
+		FamilyNameKanji: "道祖",
+		GivenNameKanji:  "克理",
+		FamilyNameKana:  "さいのう",
+		GivenNameKana:   "かつとし",
+		FamilyNameEn:    "Saino",
+		GivenNameEn:     "Katsutoshi",
+		Nickname:        "sainu",
+		ImageURL:        "/images/profileImage.jpg",
+		Job:             "Software Program Developer",
+		Email:           "katsutoshi.saino@gmail.com",
+		Bio:             "東京都出身のエンジニア。大学在学中にインターンや受託でWeb開発を経験。その後、個人開発を経て、お金の会社に就職。Railsでのサーバー開発から始まり、JSのFW(AngularやNuxt)を使ったフロントエンド開発、AWSでインフラの構築を経験してきました。",
+		Location:        "Tokyo",
+	}
+}
