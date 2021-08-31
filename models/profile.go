37bae71b78dd5897122ee2db863fcef21e856218
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-08-31 20:36:29 +0900

    Update profile data

--- models/profile.go
+++ models/profile.go
@@ -27,9 +27,9 @@ func GetProfile() *Profile {
 		FamilyNameEn:    "Saino",
 		GivenNameEn:     "Katsutoshi",
 		Nickname:        "sainu",
-		Job:             "Software Program Developer",
+		Job:             "Software Developer",
 		Email:           "katsutoshi.saino@gmail.com",
-		Bio:             "東京都出身のエンジニア。大学在学中にインターンや受託でWeb開発を経験。その後、個人開発を経て、お金の会社に就職。Railsでのサーバー開発から始まり、JSのFW(AngularやNuxt)を使ったフロントエンド開発、AWSでインフラの構築を経験してきました。",
+		Bio:             "東京都出身のエンジニア。大学在学中にインターンや受託でWeb開発を始める。その後、フリーランスを経て、マネーフォワードに入社。",
 		Location:        "Tokyo",
 	}
 }
