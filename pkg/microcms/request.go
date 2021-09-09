d55f3388a06f33ba2314d0b4b83a50491bff8a83
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-09-09 21:15:38 +0900

    Create WithQuery for options of microcms.NewRequest()

--- pkg/microcms/request.go
+++ pkg/microcms/request.go
@@ -14,6 +14,24 @@ type option interface {
 	apply(*http.Request)
 }
 
+type queryOption map[string]string
+
+func (o queryOption) apply(req *http.Request) {
+	query := req.URL.Query()
+	for k, v := range o {
+		query.Add(k, v)
+	}
+	req.URL.RawQuery = query.Encode()
+}
+
+func WithQuery(v map[string]string) queryOption {
+	var o queryOption = make(map[string]string)
+	for k, v := range v {
+		o[k] = v
+	}
+	return o
+}
+
 func NewRequest(method string, path string, options ...option) *http.Request {
 	req, _ := http.NewRequest(
 		method,
--- pkg/microcms/request_test.go
+++ pkg/microcms/request_test.go
@@ -7,9 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestWithQuery(t *testing.T) {
+	actual := WithQuery(map[string]string{"key1": "val1", "key2": "val2"})
+	expected := queryOption{"key1": "val1", "key2": "val2"}
+	assert.Equal(t, expected, actual)
+}
+
 func TestNewRequest(t *testing.T) {
 	t.Run("it set X-API-KEY header", func(t *testing.T) {
 		req := NewRequest(http.MethodGet, "/path")
 		assert.Equal(t, apiKey, req.Header.Get("X-API-KEY"))
+	})
+
+	t.Run("it set query", func(t *testing.T) {
+		req := NewRequest(
+			http.MethodGet,
+			"/path",
+			WithQuery(map[string]string{"key1": "val1", "key2": "val2"}),
+		)
+		assert.Equal(t, "key1=val1&key2=val2", req.URL.RawQuery)
 	})
 }
